fe1e8d9ec3019a44bfe23ff152e1932355dcc201
Adi-K-Coding <league@WTS10.attlocal.net>
2018-10-21 15:55:50 -0700

    Dueling Buttons, Fortune Cookie(IP)

--- src/DuelingButtons.java
+++ src/DuelingButtons.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 1
@@ -12,60 +13,68 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class DuelingButtons implements ActionListener {
-	
+
 	public static void main(String[] args) {
 		new DuelingButtons().createUI();
 	}
 
 	JButton leftButton = new JButton();
 	JButton rightButton = new JButton();
-	
-	Dimension BIG = new Dimension(400,400);
-	Dimension SMALL = new Dimension(200,200);
-	
+
+	Dimension BIG = new Dimension(400, 400);
+	Dimension SMALL = new Dimension(200, 200);
+
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 
 	private void createUI() {
 		// 1. Add the panel to the frame
-frame.add(panel);
+		frame.add(panel);
 		// 2. Make the frame visible
-frame.setVisible(true);
+		frame.setVisible(true);
+		frame.setSize(600, 400);
 		// 3. Set the text of the leftButton to "Click me!"
-leftButton.setText("Click Me!");
+		leftButton.setText("Click Me!");
 		// 4. Set the text of the rightButton to "Click me!"
-rightButton.setText("Click Me!");
+		rightButton.setText("Click Me!");
 		// 5. Add an action listener to the leftButton
-
+		leftButton.addActionListener(this);
 		// 6. Add an action listener to the rightButton
-
+		rightButton.addActionListener(this);
 		// 7. Add the leftButton to the panel
-
+		panel.add(leftButton);
 		// 8. Add the rightButton to the panel
-
+		panel.add(rightButton);
 		// 9. Pack the frame
-
+		// NO!!!!!!Why u may ask?? BECAUSE I said SO!
 		// 10. Set the title of the frame to "Demanding Buttons"
-
+		frame.setTitle("Demanding Buttons");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		JButton buttonPressed = (JButton) arg0.getSource();
-		
-		
-		/* If the buttonPressed was the leftButton....*/
-			// Set the text of the rightButton to "No, click Me!"
-			// Set the PREFERRED size of the rightButton to BIG
-			// Set the text of the leftButton to "Click Me!"
-			// Set the PREFERRED size of the leftButton to SMALL
-		
-		
-		/* If the buttonPressed was the rightButton, do the opposite. */
-		
 
-		frame.pack();
+		if (buttonPressed == leftButton) {
+			rightButton.setText("NO!!!CLICK ME!");
+			rightButton.setPreferredSize(BIG);
+			leftButton.setText("Clik ME!");
+			leftButton.setPreferredSize(SMALL);
+		} else {
+			leftButton.setText("NO!!!CLICK ME!");
+			leftButton.setPreferredSize(BIG);
+			rightButton.setText("Clik ME!");
+			rightButton.setPreferredSize(SMALL);
+		}
+		/* If the buttonPressed was the leftButton.... */
+		// Set the text of the rightButton to "No, click Me!"
+		// Set the PREFERRED size of the rightButton to BIG
+		// Set the text of the leftButton to "Click Me!"
+		// Set the PREFERRED size of the leftButton to SMALL
+
+		/* If the buttonPressed was the rightButton, do the opposite. */
+frame.pack();
 	}
 }
-
-
---
+++ src/FortuneCookie.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+public class FortuneCookie {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+FortuneCookie fc = new FortuneCookie();
+		fc.showButton();
+
+
+	}
+public void showButton() {
+	System.out.println("Button CLicked");
+JFrame bob= new JFrame();
+bob.setVisible(true);
+
+}
+}
